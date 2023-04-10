15da5b06354b62b75680714d3def47f67fea8e15
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-10 13:05:36 -0300

    setting default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        zero: "#efefef",
+        negative: "#111",
+        fist: "#1f2937",
+        second: "#111827",
+        third: "#0099dd",
+      },
+    },
   },
   plugins: [],
 };
