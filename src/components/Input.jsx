69c5030100b447cf9330d14f4dd105beee362c9c
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-10 19:34:27 -0300

    creating Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+const Input = ({ type, placeholder, id }) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      id={id}
+      className="w-full h-11 rounded-lg py-2 px-4 border border-gray-400 outline-none placeholder-gray-400 transition-all dark:shadow-lg dark:bg-second dark:text-gray-100 dark:border-none focus:ring-2 focus:ring-third focus:placeholder-third focus:border-none"
+    />
+  );
+};
+
+export default Input;
