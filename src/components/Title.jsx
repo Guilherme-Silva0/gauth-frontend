c8fae2677591bb18ab5651d1defc57a035d62629
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-10 19:34:59 -0300

    creating Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+const Title = ({ children }) => {
+  return (
+    <h1 className="text-3xl mb-3 font-bold text-gray-800 dark:text-gray-200">
+      {children}
+    </h1>
+  );
+};
+
+export default Title;
