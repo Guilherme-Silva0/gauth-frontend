d49fba1b70a96b84c8785bbc466c3a93c755b5e3
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-11 19:20:30 -0300

    importing Button

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -2,6 +2,7 @@ import Main from "../../components/Main";
 import Form from "../../components/Form";
 import Input from "../../components/Input";
 import Title from "../../components/Title";
+import Button from "../../components/Button";
 
 const Register = () => {
   return (
@@ -12,6 +13,7 @@ const Register = () => {
         <Input type="email" placeholder="digite seu email" id="email" />
         <Input type="email" placeholder="digite seu email" id="email" />
         <Input type="email" placeholder="digite seu email" id="email" />
+        <Button type="submit">Concluir</Button>
       </Form>
     </Main>
   );
