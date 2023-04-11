02da54403e5bc315d788698ea7e36b11f017912f
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-11 20:35:21 -0300

    creating theme context

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "dark");
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    const oldTheme = theme === "dark" ? "light" : "dark";
+
+    root.classList.remove(oldTheme);
+    root.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
