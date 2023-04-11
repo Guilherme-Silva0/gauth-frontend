66233ad1d7e0fd0b430ab25d77e3c4490d6527ee
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-11 20:32:58 -0300

    importing ThemeButton and theme context

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import useTheme from "./hooks/useTheme";
+import ThemeButton from "./components/ThemeButton";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
     <>
+      <ThemeButton theme={theme} onClick={toggleTheme} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
