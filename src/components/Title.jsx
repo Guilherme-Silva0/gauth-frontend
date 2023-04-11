16f942c995343ee669cb4936a3a713279777a7a9
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-11 19:20:06 -0300

    modifying the text color

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,6 @@
 const Title = ({ children }) => {
   return (
-    <h1 className="text-3xl mb-3 font-bold text-gray-800 dark:text-gray-200">
+    <h1 className="text-3xl mb-3 font-bold text-fist dark:text-gray-200">
       {children}
     </h1>
   );
