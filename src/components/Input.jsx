c014dc3290239cf35c109070b56651c093511559
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-12 16:59:45 -0300

    adding inner shadow

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({ type, placeholder, id }) => {
       type={type}
       placeholder={placeholder}
       id={id}
-      className="w-full h-11 rounded-lg py-2 px-4 border border-gray-400 outline-none placeholder-gray-400 transition-all dark:shadow-lg dark:bg-second dark:text-gray-100 dark:border-none focus:ring-2 focus:ring-third focus:placeholder-third focus:border-none"
+      className="w-full h-11 rounded-lg py-2 px-4 outline-none placeholder-gray-400 transition-all shadow-inside dark:shadow-lg dark:bg-second dark:text-gray-100 dark:border-none focus:ring-2 focus:ring-third focus:placeholder-third focus:border-none"
     />
   );
 };
