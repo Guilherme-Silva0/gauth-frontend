dc0eb5cf6402ace477796b37d44302531911f34a
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-12 16:57:49 -0300

    creating IconAnimate component

---
+++ src/components/IconAnimate.jsx
@@ -0,0 +1,10 @@
+const IconAnimate = () => {
+  return (
+    <img
+      src="login-animate.svg"
+      alt="icon animate login"
+      className="-m-10 w-6/12 mx-auto max-[600px]:w-8/12"
+    />
+  );
+};
+export default IconAnimate;
