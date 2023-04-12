4df797704756209c688bcccdadcee3eb4f59dfbc
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-12 16:55:49 -0300

    creating FormGroup component

---
+++ src/components/FormGroup.jsx
@@ -0,0 +1,9 @@
+const FormGroup = ({ children }) => {
+  return (
+    <div className="w-6/12 flex justify-around items-center bg-gray-50 px-4 py-6 rounded-2xl shadow-xl transition-all dark:bg-fist 2xl:w-5/12 max-md:flex-col max-lg:w-9/12 max-[600px]:w-11/12 max-[600px]:block">
+      {children}
+    </div>
+  );
+};
+
+export default FormGroup;
