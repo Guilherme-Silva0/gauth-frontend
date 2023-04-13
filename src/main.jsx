4f3b3ae31120837e7b984f00c57796764e728bfa
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-13 17:21:20 -0300

    wrapping application in authentication context

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App";
 import "./globalStyles/index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
