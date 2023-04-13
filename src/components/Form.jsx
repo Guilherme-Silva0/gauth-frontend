9592a4e721f878a54acfff26f4c79c785538b5ee
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-13 13:27:09 -0300

    validating registration

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,9 @@
 const Form = (props) => {
   return (
-    <form className="flex m-auto flex-col gap-3 items-center max-[600px]:w-10/12">
+    <form
+      onSubmit={(e) => props.onSubmit(e)}
+      className="flex m-auto flex-col gap-3 items-center max-[600px]:w-10/12"
+    >
       {props.children}
     </form>
   );
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import * as yup from "yup";
 import {
   Main,
   Form,
@@ -12,22 +14,64 @@ import {
   Error,
 } from "../../components";
 
+const schema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+  repeatpassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("Repeat password is required"),
+});
+
 const Register = () => {
+  const [errors, setErrors] = useState({});
+
+  const handleFormSubmit = async (values) => {
+    try {
+      setErrors({});
+      await schema.validate(values, { abortEarly: false });
+      // Submit the form data if validation succeeds
+      alert("success");
+    } catch (err) {
+      const newErrors = {};
+      err.inner.forEach((e) => {
+        newErrors[e.path] = e.message;
+      });
+      setErrors(newErrors);
+    }
+  };
+
   return (
     <Main>
       <FormGroup>
         <IconAnimate />
-        <Form>
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const { name, email, password, repeatpassword } = e.target.elements;
+            handleFormSubmit({
+              name: name.value,
+              email: email.value,
+              password: password.value,
+              repeatpassword: repeatpassword.value,
+            });
+          }}
+        >
           <Title>Create an account</Title>
           {/* <GeneralError>Teste de erro</GeneralError> */}
           <InputGroup>
             <Label htmlFor="name">Name:</Label>
             <Input type="text" placeholder="Type your name..." id="name" />
+            {errors.name && <Error>{errors.name}</Error>}
           </InputGroup>
           <InputGroup>
             <Label htmlFor="email">E-mail:</Label>
             <Input type="email" placeholder="Enter your E-mail..." id="email" />
-            {/* <Error>Insira um e-mail válido!</Error> */}
+            {errors.email && <Error>{errors.email}</Error>}
           </InputGroup>
           <InputGroup>
             <Label htmlFor="password">Password:</Label>
@@ -36,6 +80,7 @@ const Register = () => {
               placeholder="Enter a password..."
               id="password"
             />
+            {errors.password && <Error>{errors.password}</Error>}
           </InputGroup>
           <InputGroup>
             <Label htmlFor="repeatpassword">Repeat password:</Label>
@@ -44,6 +89,7 @@ const Register = () => {
               placeholder="Enter password repeat..."
               id="repeatpassword"
             />
+            {errors.repeatpassword && <Error>{errors.repeatpassword}</Error>}
           </InputGroup>
           <Button type="submit">Sign up</Button>
         </Form>
