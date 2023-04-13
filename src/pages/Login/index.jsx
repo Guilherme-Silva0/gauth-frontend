c219ec11758b49920200f964beaf78ed2ece2e14
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-13 12:01:01 -0300

    setting up form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,40 @@
+import {
+  Main,
+  Form,
+  Input,
+  Title,
+  Button,
+  Label,
+  InputGroup,
+  IconAnimate,
+  FormGroup,
+  GeneralError,
+} from "../../components";
+
 const Login = () => {
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <Main>
+      <FormGroup>
+        <IconAnimate />
+        <Form>
+          <Title>Login</Title>
+          <GeneralError>E-mail or password incorrect</GeneralError>
+          <InputGroup>
+            <Label htmlFor="email">E-Mail</Label>
+            <Input type="email" id="email" placeholder="Enter your E-mail..." />
+          </InputGroup>
+          <InputGroup>
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              id="password"
+              placeholder="Enter your password..."
+            />
+          </InputGroup>
+          <Button type="submit">Login</Button>
+        </Form>
+      </FormGroup>
+    </Main>
   );
 };
 
