11e56a6c07c5f791fe8bb24d448c3217c58e235f
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-14 18:13:00 -0300

    adding disabled property

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
-const Button = ({ children, type }) => {
+const Button = ({ children, type, disabled }) => {
   return (
     <button
       type={type}
-      className="bg-fist text-white font-semibold py-2 px-9 rounded-lg shadow-md cursor-pointer transition-all dark:bg-third dark:text-fist hover:opacity-70 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 disabled:bg-slate-600 disabled:cursor-default"
+      disabled={disabled}
+      className="bg-fist text-white font-semibold py-2 px-9 rounded-lg shadow-md cursor-pointer transition-all dark:bg-third dark:text-fist hover:opacity-70 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 dark:disabled:bg-slate-600 disabled:bg-slate-600 disabled:opacity-50 disabled:cursor-default"
     >
       {children}
     </button>
