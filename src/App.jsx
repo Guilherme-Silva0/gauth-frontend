b00a0c2e73e58f54e09d7afb7bc962525340f827
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-15 18:19:22 -0300

    creating route thanks

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Thanks from "./pages/Thanks";
 import useTheme from "./hooks/useTheme";
 import { ThemeButton } from "./components";
 
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/thanks" element={<Thanks />} />
       </Routes>
     </>
   );
