fd43155af87fc7f21a07bf9d239f2699ac8a66e8
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-15 18:21:14 -0300

    adding additional classes

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,8 @@
-const Title = ({ children }) => {
+const Title = ({ children, additionalClass }) => {
   return (
-    <h1 className="text-2xl mb-3 font-bold text-fist dark:text-gray-200">
+    <h1
+      className={`text-2xl mb-3 font-bold text-fist ${additionalClass} dark:text-gray-200`}
+    >
       {children}
     </h1>
   );
