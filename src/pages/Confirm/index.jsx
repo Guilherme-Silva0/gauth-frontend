090626b509afb273c97b187acd3b637220f11a89
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-16 14:59:58 -0300

    dealing with code confirmation

--- src/pages/Confirm/index.jsx
+++ src/pages/Confirm/index.jsx
@@ -1,10 +1,38 @@
-import { Load, Main, Title } from "../../components";
+import { useEffect, useState } from "react";
+import { Error, Load, Main, Title } from "../../components";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Confirm = () => {
+  const [error, setError] = useState("");
+  const { confirmCode, setUser } = useAuth();
+  const { confirmation_code } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    handleConfirm(confirmation_code);
+  }, []);
+
+  const handleConfirm = async (confirmation_code) => {
+    const res = await confirmCode(confirmation_code);
+    if (res.error === true) {
+      setError(res.message);
+    } else {
+      setError("");
+      setUser(res.user);
+      navigate("/");
+    }
+  };
   return (
     <Main>
-      <Title>Wait, we are confirming your email!</Title>
-      <Load />
+      {error ? (
+        <Error>{error}!</Error>
+      ) : (
+        <>
+          <Title>Wait, we are confirming your email!</Title>
+          <Load />
+        </>
+      )}
     </Main>
   );
 };
