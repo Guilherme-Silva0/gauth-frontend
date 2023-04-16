6fde870f7e55a09d9a77010233a50b159f8a6447
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-16 12:54:31 -0300

    creating confirmation page

---
+++ src/pages/Confirm/index.jsx
@@ -0,0 +1,12 @@
+import { Load, Main, Title } from "../../components";
+
+const Confirm = () => {
+  return (
+    <Main>
+      <Title>Wait, we are confirming your email!</Title>
+      <Load />
+    </Main>
+  );
+};
+
+export default Confirm;
