119b2de8ab7824185f231ece47f0ef565ffd1ae4
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-16 14:56:44 -0300

    preventing translation of links

--- src/pages/Thanks/index.jsx
+++ src/pages/Thanks/index.jsx
@@ -19,6 +19,7 @@ const Thanks = () => {
             <a
               href="https://mail.google.com/"
               target="_blank"
+              translate="no"
               className="flex items-center gap-2 px-4 py-2 bg-red-500 text-gray-200 rounded text-sm font-bold shadow transition-all hover:scale-110"
             >
               <SiGmail className="text-lg" /> Gmail
@@ -26,6 +27,7 @@ const Thanks = () => {
             <a
               href="https://outlook.live.com/"
               target="_blank"
+              translate="no"
               className="flex items-center gap-2 px-4 py-2 bg-blue-600 rounded text-gray-200 text-sm font-bold shadow transition-all hover:scale-110"
             >
               <SiMicrosoftoutlook className="text-lg" /> Outlook
