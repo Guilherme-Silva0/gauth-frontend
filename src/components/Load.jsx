e9d161ab2a04e9643bcc822b94a185aa9b7833d7
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-16 12:54:07 -0300

    theme settings

--- src/components/Load.jsx
+++ src/components/Load.jsx
@@ -1,6 +1,6 @@
 const Load = () => {
   return (
-    <div className="border-4 w-6 h-6 rounded-full border-t-transparent animate-spin"></div>
+    <div className="border-4 w-6 h-6 border-fist rounded-full border-t-transparent animate-spin dark:border-gray-200 dark:border-t-transparent"></div>
   );
 };
 
