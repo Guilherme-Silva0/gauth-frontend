43476bc0453980b0ecf8332e21ac1b4a4c8f17ba
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-19 16:07:10 -0300

    adjusting size

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ children, type, disabled }) => {
     <button
       type={type}
       disabled={disabled}
-      className="bg-fist text-white font-semibold py-2 px-9 rounded-lg shadow-md cursor-pointer transition-all dark:bg-third dark:text-fist hover:opacity-70 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 dark:disabled:bg-slate-600 disabled:bg-slate-600 disabled:opacity-50 disabled:cursor-default"
+      className="bg-fist text-white font-semibold py-2 w-full rounded-lg shadow-md cursor-pointer transition-all dark:bg-third dark:text-fist hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 dark:disabled:bg-slate-600 disabled:bg-slate-600 disabled:opacity-50 disabled:cursor-default"
     >
       {children}
     </button>
